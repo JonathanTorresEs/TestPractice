a2bf309d92431f430967d91c407d065a6c20dd47
Jonathan Torres <Jonathan Torres>
2017-03-13 19:17:40 -0700

    Segundo Commit

---
+++ UnitTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        public static long Hash(string s)
+        {
+            long h = 0;
+            var letters = "acdegilmnoprstuw";
+            var index = 0;
+            long inc = 0;
+
+            h = 7;
+
+            for (int i = 0; i < s.Length; i++)
+            { 
+                inc = h * 37;
+                index = letters.IndexOf(s[i]);
+                h = inc + index;
+            }
+
+           return h;
+        }
+
+
+    }
+}
---
+++ UnitTestTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void HashTest()
+        {
+            Assert.AreEqual(Program.Hash("leepadg"), 680131659347);
+        }
+    }
+}
